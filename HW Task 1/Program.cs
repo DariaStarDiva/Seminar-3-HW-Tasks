39aa3fffcdf5c7aa4483eb48665ae41df0676696
Petr Ivanischev <petralexiv@yandex.ru>
2022-10-10 14:59:04 +0300

    Seminar 3 HW Tasks

---
+++ HW Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+// 23432 -> да
+Console.Clear();
+Console.Write("Введите пятизначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+while ( n<10000 || n > 99999)
+{
+Console.Write("Введите пятизначное число ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+int n4 = (n%100-n/1000);
+
+if ( n4  < 0 )
+ n4 = n4 *(-1);
+if ( n4 == 9)
+Console.Write("Да, полиандром");
+else
+Console.Write("НЕТ!");
+
+
